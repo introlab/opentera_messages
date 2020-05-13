fd9a423b0b4139ec49ae076a49928b0014cbd2f4
Dominic Létourneau <doumdi@gmail.com>
2020-05-13 14:34:18 -0400

    updated TeraEvent

---
+++ python/__init__.py
@@ -0,0 +1,17 @@
+from .CreateSession_pb2 import CreateSession
+from .DeviceEvent_pb2 import DeviceEvent
+from .JoinSessionEvent_pb2 import JoinSessionEvent
+from .ParticipantEvent_pb2 import ParticipantEvent
+from .Result_pb2 import Result
+from .RPCMessage_pb2 import RPCMessage
+from .ServerCommand_pb2 import ServerCommand
+from .StopSessionEvent_pb2 import StopSessionEvent
+from .TeraEvent_pb2 import TeraEvent
+from .TeraMessage_pb2 import TeraMessage
+from .UserEvent_pb2 import UserEvent
+from .UserRegisterToEvent_pb2 import UserRegisterToEvent
+from google.protobuf.any_pb2 import Any
+
+# All exported symbols
+__all__ = ['CreateSession', 'DeviceEvent', 'JoinSessionEvent', 'ParticipantEvent', 'Result', 'RPCMessage',
+           'ServerCommand', 'StopSessionEvent', 'TeraEvent', 'TeraMessage', 'UserEvent', 'UserRegisterToEvent', 'Any']
